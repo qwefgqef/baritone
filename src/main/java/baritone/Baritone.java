20c8a04031d514902a7f79a4a9d19b107ded614e
qwefgqef <sonianevermindsalon@gmail.com>
2025-12-26 20:02:22 +0100

    Update Baritone.java

--- src/main/java/baritone/Baritone.java
+++ src/main/java/baritone/Baritone.java
@@ -111,6 +111,7 @@ public class Baritone implements IBaritone {
             this.inventoryBehavior    = this.registerBehavior(InventoryBehavior::new);
             this.inputOverrideHandler = this.registerBehavior(InputOverrideHandler::new);
             this.registerBehavior(WaypointBehavior::new);
+            this.registerBehavior(HelloWorldBehavior::new);
         }
 
         this.pathingControlManager = new PathingControlManager(this);
